183f180c6f7086ec228ac5ccf4743c34c5b5995b
Sam Flomenberg <sam.flomenberg@gmail.com>
2021-05-23 17:31:34 -0400

    feat(emacs): show magit delete and rename

--- emacs/.emacs.d/transient/levels.el
+++ emacs/.emacs.d/transient/levels.el
@@ -1,4 +1,7 @@
-((magit-log
+((magit-file-dispatch
+  (magit-file-delete . 1)
+  (magit-file-rename . 1))
+ (magit-log
   (transient:magit-log:--first-parent . 1))
  (magit-pull
   (transient:magit-pull:--autostash . 1)))
