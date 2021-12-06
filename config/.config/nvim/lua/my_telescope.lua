2b2f35cc71bed731455d20cebbb60430c22d069d
Sam Flomenberg <sam.flomenberg@gmail.com>
2021-12-05 23:13:45 -0500

    feat(vim): add git_files map

--- config/.config/nvim/lua/my_telescope.lua
+++ config/.config/nvim/lua/my_telescope.lua
@@ -3,5 +3,6 @@ local function set_keymap(...) vim.api.nvim_set_keymap(...) end
 local opts = { noremap=true, silent=true }
 
 set_keymap('n', '<Leader>f', "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<cr>", opts)
+set_keymap('n', '<Leader>g', "<cmd>lua require('telescope.builtin').git_files()<cr>", opts)
 set_keymap('n', '<Leader>b', "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 
