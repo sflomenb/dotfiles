fbc43831cca8e067fc27c0056989066b093f9295
Sam Flomenberg <sam.flomenberg@gmail.com>
2021-12-31 13:08:32 -0500

    fix(vim): remove lines causing snip error message

--- config/.config/nvim/lua/snip.lua
+++ config/.config/nvim/lua/snip.lua
@@ -43,34 +43,7 @@ rec_ls = function()
 	)
 end
 
---[[
--- Beside defining your own snippets you can also load snippets from "vscode-like" packages
--- that expose snippets in json files, for example <https://github.com/rafamadriz/friendly-snippets>.
--- Mind that this will extend  `ls.snippets` so you need to do it after your own snippets or you
--- will need to extend the table yourself instead of setting a new one.
-]]
-
--- require("luasnip.loaders.from_vscode").load({ include = { "python", "rust", "typescript" } }) -- Load only python snippets
--- -- The directories will have to be structured like eg. <https://github.com/rafamadriz/friendly-snippets> (include
--- -- a similar `package.json`)
--- require("luasnip.loaders.from_vscode").load({ paths = { "./my-snippets" } }) -- Load snippets from my-snippets folder
-
--- You can also use lazy loading so you only get in memory snippets of languages you use
-require("luasnip.loaders.from_vscode").lazy_load() -- You can pass { paths = "./my-snippets/"} as well
-
--- You can also use snippets in snipmate format, for example <https://github.com/honza/vim-snippets>.
--- The usage is similar to vscode.
-
--- One peculiarity of honza/vim-snippets is that the file with the global snippets is _.snippets, so global snippets
--- are stored in `ls.snippets._`.
--- We need to tell luasnip that "_" contains global snippets:
 ls.filetype_extend("all", { "_" })
-
-require("luasnip.loaders.from_snipmate").load({ include = { "c" } }) -- Load only python snippets
-
-require("luasnip.loaders.from_snipmate").load({ path = { "./my-snippets" } }) -- Load snippets from my-snippets folder
--- If path is not specified, luasnip will look for the `snippets` directory in rtp (for custom-snippet probably
--- `~/.config/nvim/snippets`).
 
 require("luasnip.loaders.from_snipmate").lazy_load() -- Lazy loading
 
