2477956f367c2d467084780196f3aa8c1190ae6d
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-01-15 12:03:54 -0500

    fix: prevent multiple f's from being inserted for f-string
    In some cases, the autocommand gets triggered multiple times for some
    reason. To work around this, I am saving the last changed text in a
    variable and only changing the text if this variable is not the same
    as the current text.

--- config/.config/nvim/lua/auto-change/python-f-strings.lua
+++ config/.config/nvim/lua/auto-change/python-f-strings.lua
@@ -1,36 +1,49 @@
-local ts_utils = require('nvim-treesitter.ts_utils')
-local utils = require('auto-change.utils')
+local ts_utils = require("nvim-treesitter.ts_utils")
+local utils = require("auto-change.utils")
 local M = {}
 
+LAST_TEXT = ""
+
 function M.pythonFString()
-    local lang = vim.bo.ft
-    if lang == 'python' then
-        local bufnr = vim.fn.bufnr('%')
-        local current_node = ts_utils.get_node_at_cursor()
-        if not current_node then
-            print('Unable to get current node')
-            return
-        end
+	local lang = vim.bo.ft
+	if lang ~= "python" then
+		return
+	end
 
-        local node_text = vim.treesitter.query.get_node_text(current_node, bufnr)
+	local bufnr = vim.fn.bufnr("%")
+	local current_node = ts_utils.get_node_at_cursor()
+	if not current_node then
+		print("Unable to get current node")
+		return
+	end
 
-        local isString = current_node:type() == 'string'
-        if not isString then
-            return
-        end
-        local hasCurlyBrace = string.find(node_text, '{')
-        local isFString = string.find(node_text, '^f')
+	local node_text = vim.treesitter.query.get_node_text(current_node, bufnr)
 
-        if hasCurlyBrace and not isFString then
-            local sRow, sCol, _, _ = current_node:range()
-            utils.add_character_at_pos(sRow, sCol, 'f')
-            vim.cmd([[normal l]])
-        elseif not hasCurlyBrace and isFString then
-            local sRow, sCol, _, _ = current_node:range()
-            utils.remove_character_at_pos(sRow, sCol)
-            vim.cmd([[normal h]])
-        end
-    end
+	-- HACK: Prevent multple f's from being inserted by checking last text
+	if node_text == LAST_TEXT then
+		return
+	end
+
+	LAST_TEXT = node_text
+
+	local isString = current_node:type() == "string"
+	if not isString then
+		return
+	end
+	print("in string:" .. node_text)
+	local hasCurlyBrace = string.find(node_text, "{")
+	local isFString = string.find(node_text, "^f")
+
+	if hasCurlyBrace and not isFString then
+		local sRow, sCol, _, _ = current_node:range()
+		print("adding f at " .. sRow .. ", " .. sCol)
+		utils.add_character_at_pos(sRow, sCol, "f")
+		vim.cmd([[normal l]])
+	elseif not hasCurlyBrace and isFString then
+		local sRow, sCol, _, _ = current_node:range()
+		utils.remove_character_at_pos(sRow, sCol)
+		vim.cmd([[normal h]])
+	end
 end
 
 return M
