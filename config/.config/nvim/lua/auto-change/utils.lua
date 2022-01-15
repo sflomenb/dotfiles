18ffcd1e8b05fedc92c4800fcaf3532906b70a87
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-01-15 12:01:42 -0500

    style: auto-format auto-change/utils.lua

--- config/.config/nvim/lua/auto-change/utils.lua
+++ config/.config/nvim/lua/auto-change/utils.lua
@@ -1,24 +1,24 @@
 local M = {}
 
 function M.add_character_at_pos(row, col, char)
-    local view = vim.fn.winsaveview()
-    vim.api.nvim_win_set_cursor(0, {row+1, col})
-    vim.cmd('normal i' .. char)
-    vim.fn.winrestview(view)
+	local view = vim.fn.winsaveview()
+	vim.api.nvim_win_set_cursor(0, { row + 1, col })
+	vim.cmd("normal i" .. char)
+	vim.fn.winrestview(view)
 end
 
 function M.remove_character_at_pos(row, col)
-    local view = vim.fn.winsaveview()
-    vim.api.nvim_win_set_cursor(0, {row+1, col})
-    vim.cmd([[normal x]])
-    vim.fn.winrestview(view)
+	local view = vim.fn.winsaveview()
+	vim.api.nvim_win_set_cursor(0, { row + 1, col })
+	vim.cmd([[normal x]])
+	vim.fn.winrestview(view)
 end
 
 function M.change_character_at_pos(row, col, char)
-    local view = vim.fn.winsaveview()
-    vim.api.nvim_win_set_cursor(0, {row+1, col})
-    vim.cmd('normal r' .. char)
-    vim.fn.winrestview(view)
+	local view = vim.fn.winsaveview()
+	vim.api.nvim_win_set_cursor(0, { row + 1, col })
+	vim.cmd("normal r" .. char)
+	vim.fn.winrestview(view)
 end
 
 return M
