4a0b5cf662a72a532d5d87a2e5c10334c15db2a3
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-03-20 13:37:36 -0400

    style(vim): auto format ts-js-backticks.lua

--- config/.config/nvim/lua/auto-change/ts-js-backticks.lua
+++ config/.config/nvim/lua/auto-change/ts-js-backticks.lua
@@ -1,43 +1,43 @@
-local ts_utils = require('nvim-treesitter.ts_utils')
-local utils = require('auto-change.utils')
+local ts_utils = require("nvim-treesitter.ts_utils")
+local utils = require("auto-change.utils")
 local M = {}
 
 function M.tsJsBackticks()
-    local lang = vim.bo.ft
-    if lang == 'javascript' or lang == 'typescript' then
-        local bufnr = vim.fn.bufnr('%')
-        local current_node = ts_utils.get_node_at_cursor()
-        if not current_node then
-            print('Unable to get current node')
-            return
-        end
+	local lang = vim.bo.ft
+	if lang == "javascript" or lang == "typescript" then
+		local bufnr = vim.fn.bufnr("%")
+		local current_node = ts_utils.get_node_at_cursor()
+		if not current_node then
+			print("Unable to get current node")
+			return
+		end
 
-        local isString = current_node:type() == 'string'
-        local isStringFragment = current_node:type() == 'string_fragment'
+		local isString = current_node:type() == "string"
+		local isStringFragment = current_node:type() == "string_fragment"
 
-        if not isString and not isStringFragment then
-            return
-        end
+		if not isString and not isStringFragment then
+			return
+		end
 
-        if isStringFragment then
-            current_node = current_node:parent()
-        end
+		if isStringFragment then
+			current_node = current_node:parent()
+		end
 
-        local node_text = vim.treesitter.query.get_node_text(current_node, bufnr)
+		local node_text = vim.treesitter.query.get_node_text(current_node, bufnr)
 
-        if not node_text then
-            return
-        end
+		if not node_text then
+			return
+		end
 
-        local hasInterpolation = string.find(node_text, '%${')
+		local hasInterpolation = string.find(node_text, "%${")
 
-        if hasInterpolation then
-            local sRow, sCol, eRow, eCol = current_node:range()
-            utils.change_character_at_pos(sRow, sCol, '`')
-            -- Subtract 1 from eCol since the close } is added automatically
-            utils.change_character_at_pos(eRow, eCol - 1, '`')
-        end
-    end
+		if hasInterpolation then
+			local sRow, sCol, eRow, eCol = current_node:range()
+			utils.change_character_at_pos(sRow, sCol, "`")
+			-- Subtract 1 from eCol since the close } is added automatically
+			utils.change_character_at_pos(eRow, eCol - 1, "`")
+		end
+	end
 end
 
 return M
