6387b18cd8129f901a21701c81904633358c95f5
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-03-20 13:38:30 -0400

    feat(vim): perform similar changes from python to js-ts
    Wrap in schedule_wrap and add undojoin

--- config/.config/nvim/lua/auto-change/ts-js-backticks.lua
+++ config/.config/nvim/lua/auto-change/ts-js-backticks.lua
@@ -3,41 +3,44 @@ local utils = require("auto-change.utils")
 local M = {}
 
 function M.tsJsBackticks()
-	local lang = vim.bo.ft
-	if lang == "javascript" or lang == "typescript" then
-		local bufnr = vim.fn.bufnr("%")
-		local current_node = ts_utils.get_node_at_cursor()
-		if not current_node then
-			print("Unable to get current node")
-			return
+	vim.schedule_wrap(function()
+		local lang = vim.bo.ft
+		if lang == "javascript" or lang == "typescript" then
+			local bufnr = vim.fn.bufnr("%")
+			local current_node = ts_utils.get_node_at_cursor()
+			if not current_node then
+				print("Unable to get current node")
+				return
+			end
+
+			local isString = current_node:type() == "string"
+			local isStringFragment = current_node:type() == "string_fragment"
+
+			if not isString and not isStringFragment then
+				return
+			end
+
+			if isStringFragment then
+				current_node = current_node:parent()
+			end
+
+			local node_text = vim.treesitter.query.get_node_text(current_node, bufnr)
+
+			if not node_text then
+				return
+			end
+
+			local hasInterpolation = string.find(node_text, "%${")
+
+			if hasInterpolation then
+				vim.cmd("silent! undojoin")
+				local sRow, sCol, eRow, eCol = current_node:range()
+				utils.change_character_at_pos(sRow, sCol, "`")
+				-- Subtract 1 from eCol since the close } is added automatically
+				utils.change_character_at_pos(eRow, eCol - 1, "`")
+			end
 		end
-
-		local isString = current_node:type() == "string"
-		local isStringFragment = current_node:type() == "string_fragment"
-
-		if not isString and not isStringFragment then
-			return
-		end
-
-		if isStringFragment then
-			current_node = current_node:parent()
-		end
-
-		local node_text = vim.treesitter.query.get_node_text(current_node, bufnr)
-
-		if not node_text then
-			return
-		end
-
-		local hasInterpolation = string.find(node_text, "%${")
-
-		if hasInterpolation then
-			local sRow, sCol, eRow, eCol = current_node:range()
-			utils.change_character_at_pos(sRow, sCol, "`")
-			-- Subtract 1 from eCol since the close } is added automatically
-			utils.change_character_at_pos(eRow, eCol - 1, "`")
-		end
-	end
+	end)()
 end
 
 return M
