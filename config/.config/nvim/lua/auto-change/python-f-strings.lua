d7a477c8699301beef36fdd5a77259ffaf6cb0e2
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-03-20 13:04:40 -0400

    fix(vim): fix python-f-string to prevent extra f's
    When undoing and redoing, extra f's were being added to the beginning
    of the string. This seems to have been fixed by using
    vim.schedule_wrap() as well as checking the node again right before
    updating it.

--- config/.config/nvim/lua/auto-change/python-f-strings.lua
+++ config/.config/nvim/lua/auto-change/python-f-strings.lua
@@ -2,49 +2,69 @@ local ts_utils = require("nvim-treesitter.ts_utils")
 local utils = require("auto-change.utils")
 local M = {}
 
-LAST_TEXT = ""
-
-function M.pythonFString()
-	local lang = vim.bo.ft
-	if lang ~= "python" then
-		return
-	end
-
-	local bufnr = vim.fn.bufnr("%")
+local function get_node_and_text(bufnr)
 	local current_node = ts_utils.get_node_at_cursor()
 	if not current_node then
 		print("Unable to get current node")
-		return
+		return {}
+	end
+	if current_node:has_error() then
+		return {}
+	end
+
+	local is_string = current_node:type() == "string"
+	if not is_string then
+		return {}
 	end
 
 	local node_text = vim.treesitter.query.get_node_text(current_node, bufnr)
 	if not node_text then
-		return
+		return {}
 	end
 
-	-- HACK: Prevent multple f's from being inserted by checking last text
-	if node_text == LAST_TEXT then
-		return
-	end
+	return { current_node, node_text }
+end
 
-	LAST_TEXT = node_text
+function M.pythonFString()
+	vim.schedule_wrap(function()
+		local lang = vim.bo.ft
+		if lang ~= "python" then
+			return
+		end
 
-	local isString = current_node:type() == "string"
-	if not isString then
-		return
-	end
-	local hasCurlyBrace = string.find(node_text, "{")
-	local isFString = string.find(node_text, "^f")
+		local bufnr = vim.fn.bufnr("%")
 
-	if hasCurlyBrace and not isFString then
-		local sRow, sCol, _, _ = current_node:range()
-		utils.add_character_at_pos(sRow, sCol, "f")
-		vim.cmd([[normal l]])
-	elseif not hasCurlyBrace and isFString then
-		local sRow, sCol, _, _ = current_node:range()
-		utils.remove_character_at_pos(sRow, sCol)
-		vim.cmd([[normal h]])
-	end
+		local current_node, node_text = unpack(get_node_and_text(bufnr))
+
+		if not current_node or not node_text then
+			return
+		end
+
+		local has_open_curly_brace = string.find(node_text, "{")
+		local is_f_string = string.find(node_text, "^f")
+
+		if has_open_curly_brace and not is_f_string then
+			current_node, node_text = unpack(get_node_and_text(bufnr))
+
+			if not current_node or not node_text then
+				return
+			end
+			local srow, scol, _, _ = current_node:range()
+			vim.cmd("silent! undojoin")
+			utils.add_character_at_pos(srow, scol, "f")
+			vim.cmd([[normal l]])
+		elseif not has_open_curly_brace and is_f_string then
+			current_node, node_text = unpack(get_node_and_text(bufnr))
+
+			if not current_node or not node_text then
+				return
+			end
+			local srow, scol, _, _ = current_node:range()
+			vim.cmd("silent! undojoin")
+			utils.remove_character_at_pos(srow, scol)
+			vim.cmd([[normal h]])
+		end
+	end)()
 end
 
 return M
