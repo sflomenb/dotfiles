ee4f69584a477807211e361c322931889f389a66
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-03-20 13:08:14 -0400

    style(vim): autoformat ts.lua

--- config/.config/nvim/lua/ts.lua
+++ config/.config/nvim/lua/ts.lua
@@ -1,24 +1,24 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "rust", "typescript", "go", "javascript", "python", "query" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    -- disable = { "c", "rust" },  -- list of language that will be disabled
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-  },
-  query_linter = {
-      enable = true,
-      use_virtual_text = true,
-      lint_events = { "BufWrite", "CursorHold" },
-  },
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "rust", "typescript", "go", "javascript", "python", "query" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		-- disable = { "c", "rust" },  -- list of language that will be disabled
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+	},
+	query_linter = {
+		enable = true,
+		use_virtual_text = true,
+		lint_events = { "BufWrite", "CursorHold" },
+	},
+})
 
