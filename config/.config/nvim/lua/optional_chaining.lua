5df7853c35f61e331375fd02d82da6b6102df029
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-06-05 09:15:40 -0400

    fix(vim): handle optional chaining for parts of line

--- config/.config/nvim/lua/optional_chaining.lua
+++ config/.config/nvim/lua/optional_chaining.lua
@@ -3,6 +3,25 @@ local treesitter = require("vim.treesitter")
 
 local M = {}
 
+local function in_range(node, pos)
+	local sRow, sCol, eRow, eCol = node:range()
+	local vis_start_row, vis_start_col, vis_end_row, vis_end_col = unpack(pos)
+	if sRow + 1 < vis_start_row then
+		return false
+	end
+	if eRow + 1 > vis_end_row then
+		return false
+	end
+
+	if sRow + 1 == vis_start_row and sCol < vis_start_col then
+		return false
+	end
+	if eRow + 1 == vis_end_row and eCol > vis_end_col then
+		return false
+	end
+	return true
+end
+
 local type_replacement = {
 	["arguments"] = [[ :normal! i?.]],
 	["."] = [[ :normal! i?]],
@@ -23,9 +42,14 @@ local function add_optional_chaining_to_node(node)
 			-- front of this node
 			if text_before ~= [[?.]] then
 				vim.cmd(cmd)
+				if type == "." then
+					return 1
+				else
+					return 2
+				end
 			end
 
-			break
+			return 0
 		end
 	end
 end
@@ -40,8 +64,8 @@ function M.add_optional_chaining()
 	-- Exit visual mode.
 	vim.cmd([[ :normal!  ]])
 
-	local vis_start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, "<"))
-	local vis_end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, ">"))
+	local vis_start_row, vis_start_col = unpack(vim.api.nvim_buf_get_mark(0, "<"))
+	local _, vis_end_row, vis_end_col, _ = unpack(vim.fn.getcharpos("'>"))
 
 	for i = vis_start_row, vis_end_row do
 		vim.api.nvim_win_set_cursor(0, { i, 0 })
@@ -52,7 +76,12 @@ function M.add_optional_chaining()
 
 			-- Lines are 0-indexed.
 			for _, node, _ in parsed_query:iter_captures(tree, 0, i - 1, -1) do
-				add_optional_chaining_to_node(node)
+				if in_range(node, { vis_start_row, vis_start_col, vis_end_row, vis_end_col }) then
+					local change_amount = add_optional_chaining_to_node(node)
+					if i == vis_start_row then
+						vis_end_col = vis_end_col + change_amount
+					end
+				end
 			end
 		end
 	end
--- config/.config/nvim/lua/tests/test_optional_chaining_spec.lua
+++ config/.config/nvim/lua/tests/test_optional_chaining_spec.lua
@@ -106,4 +106,56 @@ const meep = wow.zip;]]
 			"const meep = wow?.zip;",
 		}, vim.api.nvim_buf_get_lines(0, 0, -1, false))
 	end)
+
+	it("adds optional chaining for part of a line", function()
+		local buffer_text = [[const bar = 'hello world!';
+const foo = whee.Foo['Whee Saw'].lala.fakeMethod();
+const meep = wow.zip;]]
+		local split_string = {}
+		string.gsub(buffer_text, "([^\n]+)", function(c)
+			split_string[#split_string + 1] = c
+		end)
+
+		assert.same(split_string, {
+			"const bar = 'hello world!';",
+			"const foo = whee.Foo['Whee Saw'].lala.fakeMethod();",
+			"const meep = wow.zip;",
+		})
+
+		vim.api.nvim_buf_set_lines(0, 0, -1, false, split_string)
+		vim.api.nvim_buf_set_option(0, "filetype", "typescript")
+		vim.api.nvim_win_set_cursor(0, { 2, 0 })
+		vim.cmd([[ normal! ^fwvf[ ]])
+
+		optional_chaining.add_optional_chaining()
+
+		assert.same({
+			"const bar = 'hello world!';",
+			"const foo = whee?.Foo?.['Whee Saw'].lala.fakeMethod();",
+			"const meep = wow.zip;",
+		}, vim.api.nvim_buf_get_lines(0, 0, -1, false))
+	end)
+
+	it("does not add to if statement parenthesis", function()
+		local buffer_text = [[if(whee.Foo['Whee Saw'].lala.fakeMethod()) {}]]
+		local split_string = {}
+		string.gsub(buffer_text, "([^\n]+)", function(c)
+			split_string[#split_string + 1] = c
+		end)
+
+		assert.same(split_string, {
+			"if(whee.Foo['Whee Saw'].lala.fakeMethod()) {}",
+		})
+
+		vim.api.nvim_buf_set_lines(0, 0, -1, false, split_string)
+		vim.api.nvim_buf_set_option(0, "filetype", "typescript")
+		vim.api.nvim_win_set_cursor(0, { 1, 0 })
+		vim.cmd([[ normal! V ]])
+
+		optional_chaining.add_optional_chaining()
+
+		assert.same({
+			"if(whee?.Foo?.['Whee Saw']?.lala?.fakeMethod?.()) {}",
+		}, vim.api.nvim_buf_get_lines(0, 0, -1, false))
+	end)
 end)
