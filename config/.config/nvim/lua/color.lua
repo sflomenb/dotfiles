96d21cc7a70737789460f9234d9da4fc1fb0fb10
Sam Flomenberg <sam.flomenberg@gmail.com>
2022-10-01 22:42:13 -0400

    feat(vim): rewrite set_background_mode in lua

--- config/.config/nvim/lua/color.lua
+++ config/.config/nvim/lua/color.lua
@@ -3,11 +3,12 @@ local a = require("plenary.async")
 
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 
-local function set_color_via_macos_theme(callback)
-	local script_path =
-		"/Users/sflomenb/Library/Mobile Documents/com~apple~ScriptEditor2/Documents/Light-Dark Terminal.scpt"
+local script_path = vim.fn.expand("~/light-dark.scpt")
+local iterm2_python_path = vim.fn.expand("~/Library/ApplicationSupport/iTerm2/iterm2env/versions/3.8.6/bin/python3")
+local toggle_script_path = vim.fn.expand("~/Library/ApplicationSupport/iTerm2/Scripts/iterm2-light-dark-toggle.py")
 
-	local err, stat = a.uv.fs_stat(script_path)
+local function set_color_via_macos_theme(callback)
+	local err, _ = a.uv.fs_stat(script_path)
 	if err then
 		print("err: " .. (vim.inspect(err) or ""))
 		callback()
@@ -27,28 +28,22 @@ local function set_color_via_macos_theme(callback)
 			table.insert(results, line)
 		end,
 		on_exit = function(_, _)
+			local new_bg = "light"
 			for _, v in pairs(results) do
 				if string.lower(v) == "light" then
-					vim.g.catppuccin_flavour = "latte" -- latte, frappe, macchiato, mocha
+					new_bg = "light"
+					vim.g.catppuccin_flavour = "latte"
 					break
 				elseif string.lower(v) == "dark" then
-					vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+					new_bg = "dark"
+					vim.g.catppuccin_flavour = "macchiato"
 					break
 				end
 			end
-			callback()
+			callback(new_bg)
 		end,
 	}):start()
 end
-
-a.run(function()
-	set_color_via_macos_theme(function()
-		vim.schedule(function()
-			require("catppuccin").setup()
-			vim.cmd([[colorscheme catppuccin]])
-		end)
-	end)
-end)
 
 vim.api.nvim_create_autocmd("OptionSet", {
 	pattern = "background",
@@ -57,10 +52,95 @@ vim.api.nvim_create_autocmd("OptionSet", {
 	end,
 })
 
-if vim.v.vim_did_enter then
-	vim.defer_fn(function()
-		vim.fn["SetBackgroundMode"]()
-		vim.cmd("Catppuccin " .. (vim.o.background == "light" and "latte" or "macchiato"))
-		vim.cmd([[colorscheme catppuccin]])
-	end, 500)
+local function set_iterm_theme()
+	local err, stat = a.uv.fs_stat(iterm2_python_path)
+	if err or not stat then
+		print("err: " .. (vim.inspect(err) or ""))
+		return
+	end
+
+	err, stat = a.uv.fs_stat(toggle_script_path)
+	if err or not stat then
+		print("err: " .. (vim.inspect(err) or ""))
+		return
+	end
+
+	Job:new({
+		command = iterm2_python_path,
+		args = {
+			toggle_script_path,
+		},
+	}):start()
 end
+
+local function should_use_macos_theme()
+	--  https://www.reddit.com/r/neovim/comments/pc7in0/comment/hakuhzn/?utm_source=reddit&utm_medium=web2x&context=3
+	local is_macos = vim.loop.os_uname().sysname == "Darwin"
+
+	local err, stat = a.uv.fs_stat(script_path)
+	if err or not stat then
+		print("err: " .. (vim.inspect(err) or ""))
+		return false
+	end
+
+	return is_macos and stat
+end
+
+local function set_background_mode(callback)
+	local new_bg = "light"
+	local ENV_VAR_NAME = "VIM_BACKGROUND"
+
+	if not callback then
+		callback = function(bg)
+			vim.schedule(function()
+				if vim.opt.background:get() ~= bg then
+					vim.o.background = bg
+				end
+			end)
+		end
+	end
+
+	local vim_background_env = os.getenv(ENV_VAR_NAME)
+
+	if vim_background_env then
+		new_bg = vim_background_env == "light" and "light" or "dark"
+		callback(new_bg)
+	elseif should_use_macos_theme() then
+		set_color_via_macos_theme(callback)
+		a.run(function()
+			set_iterm_theme()
+		end)
+	else
+		-- https://stackoverflow.com/a/68830379/5521899
+		local hour = tonumber(os.date("%H"))
+		new_bg = (hour > 6 and hour < 18) and "light" or "dark"
+		callback(new_bg)
+	end
+end
+
+a.run(function()
+	set_background_mode(function()
+		vim.schedule(function()
+			require("catppuccin").setup()
+			vim.cmd([[colorscheme catppuccin]])
+		end)
+	end)
+end)
+
+local timer = vim.loop.new_timer()
+timer:start(
+	300,
+	300000,
+	vim.schedule_wrap(function()
+		a.run(function()
+			set_background_mode()
+		end)
+	end)
+)
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+	pattern = "*",
+	callback = function()
+		timer:close()
+	end,
+})
