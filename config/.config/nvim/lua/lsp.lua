620af4f46ab16ee405b9d098d7dce6d62dfc1efe
Sam Flomenberg <sam.flomenberg@gmail.com>
2023-02-11 15:25:26 -0500

    fix(vim): use default_capabilities function

--- config/.config/nvim/lua/lsp.lua
+++ config/.config/nvim/lua/lsp.lua
@@ -102,8 +102,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Set default client capabilities plus window/workDoneProgress
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities = vim.tbl_extend("keep", capabilities or {}, lsp_status.capabilities)
 
 local servers_with_default_config = { "pyright", "eslint", "terraform_lsp" }
