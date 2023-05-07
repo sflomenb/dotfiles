6c85d8f68aa97e7a10646d6fa9da2b47cf43da8a
Sam Flomenberg <sam.flomenberg@gmail.com>
2023-05-06 22:36:23 -0400

    feat(emacs): add gc-cons-threshold to early-init.el

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,5 +1,11 @@
 ;;; Code:
 (setq package-enable-at-startup nil)
 
+;; https://blog.d46.us/advanced-emacs-startup/
+;; Make startup faster by reducing the frequency of garbage
+;; collection.  The default is 800 kilobytes.  Measured in bytes.
+;; (message "increasing gc-cons-threshold from %d to %d" gc-cons-threshold (* 50 1000 1000))
+(setq gc-cons-threshold (* 50 1000 1000))
+
 (provide 'early-init)
 ;;; early-init.el ends here
