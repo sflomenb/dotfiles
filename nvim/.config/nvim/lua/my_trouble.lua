509547897e65f3ecdba87df50663906155b91932
Sam Flomenberg <sam.flomenberg@gmail.com>
2025-03-24 19:36:20 -0400

    fix(vim): update trouble maps

--- nvim/.config/nvim/lua/my_trouble.lua
+++ nvim/.config/nvim/lua/my_trouble.lua
@@ -1,6 +1,6 @@
-require("trouble").setup {}
+require("trouble").setup({})
 
-vim.api.nvim_set_keymap("n", "<leader>tl", "<cmd>lua require('trouble').toggle()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[t", "<cmd>lua require('trouble').previous({skip_groups=true, jump=true})<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "]t", "<cmd>lua require('trouble').next({skip_groups=true, jump=true})<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>tl", "<cmd>lua require('trouble').toggle('diagnostics')<CR>", { noremap = true })
+vim.keymap.set("n", "[t", "<cmd>lua require('trouble').prev({skip_groups=true, jump=true})<CR>", { noremap = true })
+vim.keymap.set("n", "]t", "<cmd>lua require('trouble').next({skip_groups=true, jump=true})<CR>", { noremap = true })
 
