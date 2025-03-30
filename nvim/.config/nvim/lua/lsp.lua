49d87dbb706a7e0c907154aef84ca46427915195
Sam Flomenberg <sam.flomenberg@gmail.com>
2025-03-30 14:20:24 -0400

    feat(vim): insert instead of replace completion

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -278,7 +278,7 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
 		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
+			behavior = cmp.ConfirmBehavior.Insert,
 			select = true,
 		}),
 		["<Tab>"] = cmp.mapping(function(fallback)
