0f135dbbf05a364e68f8cc9b18ae432829941dca
Sam Flomenberg <sam.flomenberg@gmail.com>
2025-05-08 22:22:56 -0400

    fix(vim): disable tree sitter on large files

--- nvim/.config/nvim/lua/ts.lua
+++ nvim/.config/nvim/lua/ts.lua
@@ -6,8 +6,13 @@ require("nvim-treesitter.configs").setup({
 	modules = {},
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- false will disable the whole extension
-		-- disable = { "c", "rust" },  -- list of language that will be disabled
+		disable = function(lang, buf)
+			local max_filesize = 512 * 1024
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
