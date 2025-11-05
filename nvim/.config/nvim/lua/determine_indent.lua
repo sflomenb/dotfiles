7a9276b7a9b8feee5edc7c480167fd6074e12ca6
Sam Flomenberg <sam.flomenberg@gmail.com>
2025-11-05 18:08:41 -0500

    fix(vim): ignore detected indentation for comments

--- nvim/.config/nvim/lua/determine_indent.lua
+++ nvim/.config/nvim/lua/determine_indent.lua
@@ -1,12 +1,45 @@
 local M = {}
 
+local function treesitter_enabled()
+	return vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()]
+end
+
 function M.determine_indent()
 	local res = vim.api.nvim_buf_get_lines(0, 0, 100, false)
-	for _, line in pairs(res) do
+
+	local is_treesitter_enabled = treesitter_enabled()
+	if is_treesitter_enabled then
+		-- Ensure the buffer is parsed.
+		vim.treesitter.get_parser(0):parse()
+	end
+
+	for line_num, line in pairs(res) do
 		local found_space = string.match(line, "^%s+")
 
 		if not found_space then
 			goto continue
+		end
+
+		if is_treesitter_enabled then
+			-- If is comment, continue.
+			-- vim.treesitter.get_node() line nums are 0-indexed.
+			local current_node = vim.treesitter.get_node({pos = {line_num-1, 0}})
+			if current_node == nil then
+				goto continue
+			end
+
+			local is_comment = current_node:type() == "comment"
+			if is_comment then
+				goto continue
+			end
+		else
+			-- Treesitter is disabled, need to check the syntax.
+			-- https://vim.fandom.com/wiki/Check_for_comments_independent_of_filetype
+			-- If is comment, continue.
+			local is_comment = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.synID(line_num, 1, 0)), "name") == "Comment"
+			if is_comment then
+				goto continue
+			end
 		end
 
 		if found_space:find("^%t") or not found_space:find("^ ") then
--- nvim/.config/nvim/lua/tests/determine_indent_spec.lua
+++ nvim/.config/nvim/lua/tests/determine_indent_spec.lua
@@ -3,7 +3,7 @@ local determine_indent = require("determine_indent")
 describe("determine_indent", function()
 	it("detects 2 space indentation", function()
 		vim.api.nvim_buf_set_lines(0, 0, -1, false, { "import foo from bar;", "  console.log('foo');" })
-		vim.api.nvim_buf_set_option(0, "filetype", "javascript")
+		vim.api.nvim_set_option_value("filetype", "javascript", {buf = 0})
 		vim.api.nvim_win_set_cursor(0, { 1, 0 })
 
 		local actual = determine_indent.determine_indent()
@@ -13,7 +13,28 @@ describe("determine_indent", function()
 
 	it("detects 4 space indentation", function()
 		vim.api.nvim_buf_set_lines(0, 0, -1, false, { "import foo from bar;", "    console.log('foo');" })
-		vim.api.nvim_buf_set_option(0, "filetype", "javascript")
+		vim.api.nvim_set_option_value("filetype", "javascript", {buf = 0})
+		vim.api.nvim_win_set_cursor(0, { 1, 0 })
+
+		local actual = determine_indent.determine_indent()
+
+		assert.same(4, actual)
+	end)
+
+	it("ignores comments without treesitter", function()
+		vim.api.nvim_buf_set_lines(0, 0, -1, false, { "import foo from bar;", "/**", " * Some comment.", " */", "    console.log('foo');" })
+		vim.api.nvim_set_option_value("filetype", "javascript", {buf = 0})
+		vim.cmd([[TSBufDisable highlight]])
+		vim.api.nvim_win_set_cursor(0, { 1, 0 })
+
+		local actual = determine_indent.determine_indent()
+
+		assert.same(4, actual)
+	end)
+
+	it("ignores comments with treesitter", function()
+		vim.api.nvim_buf_set_lines(0, 0, -1, false, { "import foo from bar;", "/**", " * Some comment.", " */", "    console.log('foo');" })
+		vim.api.nvim_set_option_value("filetype", "javascript", {buf = 0})
 		vim.api.nvim_win_set_cursor(0, { 1, 0 })
 
 		local actual = determine_indent.determine_indent()
@@ -23,7 +44,7 @@ describe("determine_indent", function()
 
 	it("returns 0 when no indentation found", function()
 		vim.api.nvim_buf_set_lines(0, 0, -1, false, { "import foo from bar;" })
-		vim.api.nvim_buf_set_option(0, "filetype", "javascript")
+		vim.api.nvim_set_option_value("filetype", "javascript", {buf = 0})
 		vim.api.nvim_win_set_cursor(0, { 1, 0 })
 
 		local actual = determine_indent.determine_indent()
